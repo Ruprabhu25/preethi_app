5d9ba7be6e0a60d4da2e985515c1c664c6183885
Rahul Prabhu <98982995+Ruprabhu25@users.noreply.github.com>
2023-11-09 17:28:39 -0800

    first commit

---
+++ pages/homepage/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import exComponent from "../../components/ex_component"
+
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1 style={{}} className="text-3xl font-bold underline" >
+                Hello world!
+            </h1>
+        </div>
+    )
+}
