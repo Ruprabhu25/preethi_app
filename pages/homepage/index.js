286de825ae0c4906118fcf00517b161ec5ce81b2
DaphneLoustalet <114203018+DaphneLoustalet@users.noreply.github.com>
2023-11-11 22:23:23 -0800

    Added Footer Component

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -1,13 +1,14 @@
-import React from "react"
-import exComponent from "../../components/ex_component"
-
+import React from "react";
+import exComponent from "../../components/ex_component";
+import Footer from "../../components/Footer";
 
 export default function HomePage() {
-    return (
-        <div>
-            <h1 style={{}} className="text-3xl font-bold underline" >
-                Hello world!
-            </h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 style={{}} className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Footer />
+    </div>
+  );
 }
