d9e5fa3c8c8e06327ddc2564ce53da42392b617c
DaphneLoustalet <114203018+DaphneLoustalet@users.noreply.github.com>
2023-11-19 15:25:20 -0800

    Added AboutUS

---
+++ components/AboutUS.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../../../public/global.css";
+
+export default function AboutUS() {
+  return (
+    <div class="about_us">
+            <div class="left-half">
+            <img src="our_story_preface.png" />
+            <div class="ourstory">OUR STORY</div>
+            <div class="title">About us</div>
+            <div class="text">
+                We are a family owned restaurant with over 25 years of experience
+                creating authentic, southern and northern regional dishes that honor
+                traditional centuries-old family recipes.
+            </div>
+            <div class="text">
+                We offer a daily lunch buffet full of deliciously familiar dishes
+                including Dosas, and an intriguing and diverse dinner menu full of
+                homemade regional cuisines that reflect the natural ethnic mixture
+                of authentic spices and flavors of each area.
+            </div>
+            </div>
+            <div class="right-half">
+            <img class="chopped_greens" , src="chopped_greens.png" />
+            <img class="sliced_tomato" , src="slicing_tomato.png" />
+            <img class="chopped_greens1" , src="Ellipse 5.png" />
+            <img class="sliced_tomato1" , src="Ellipse 5.png" />
+            <img class="chopped_greens2" , src="curly_frame.png" />
+            <img class="sliced_tomato2" , src="curly_frame.png" />
+            </div>
+    </div>
+  );
+  }
